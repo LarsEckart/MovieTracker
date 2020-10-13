8ea034b486487104c7184de561e028098d1c9f0c
Lars Eckart <lars.eckart@gmail.com>
2020-10-13 13:46:08 +0300

    exercise

--- src/main/java/com/larseckart/movietracker/Category.java
+++ src/main/java/com/larseckart/movietracker/Category.java
@@ -11,4 +11,8 @@ class Category {
   public static final Category UNCATEGORIZED = new Category("Uncategorized");
   public static final Category SCIFI = new Category("Science Fiction");
   public static final Category HORROR = new Category("Horror");
+  public static final Category COMEDY = new Category("Comedy");
+  public static final Category DRAMA = new Category("Drama");
+  public static final Category Kids = new Category("Kids");
+  public static final Category THRILLER = new Category("Thriller");
 }
