8a08f997abd4f217515d7aec1bd78ff011716200
Lars Eckart <lars.eckart@gmail.com>
2020-10-15 15:49:37 +0300

    refactor

--- src/main/java/com/larseckart/movietracker/XMLMovieListWriter.java
+++ src/main/java/com/larseckart/movietracker/XMLMovieListWriter.java
@@ -16,26 +16,34 @@ class XMLMovieListWriter implements MovieListWriter {
   public void write(MovieList movies) throws IOException {
     destination.write("<movielist>");
     if (movies.size() > 0) {
-      Movie movie = movies.getMovie(0);
-      destination.write("\n  <movie name=\"");
-      destination.write(movie.getName());
-      destination.write("\" category=\"");
-      destination.write(movie.getCategory().toString());
-      destination.write("\">");
-      destination.write("\n    <ratings>");
-      Iterator<Rating> ratings = movie.ratings();
-      while (ratings.hasNext()) {
-        destination.write("\n      <rating value=\"");
-        Rating next = ratings.next();
-        destination.write(Integer.toString(next.value()));
-        destination.write("\" source=\"");
-        destination.write(next.source());
-        destination.write("\" />");
-      }
-      destination.write("\n    </ratings>");
-      destination.write("\n  </movie>");
+      writeMovie(movies);
     }
     destination.write("\n</movielist>");
     destination.flush();
+  }
+
+  private void writeMovie(MovieList movies) throws IOException {
+    Movie movie = movies.getMovie(0);
+    destination.write("\n  <movie name=\"");
+    destination.write(movie.getName());
+    destination.write("\" category=\"");
+    destination.write(movie.getCategory().toString());
+    destination.write("\">");
+    destination.write("\n    <ratings>");
+    Iterator<Rating> ratings = movie.ratings();
+    while (ratings.hasNext()) {
+      writeRating(ratings);
+    }
+    destination.write("\n    </ratings>");
+    destination.write("\n  </movie>");
+  }
+
+  private void writeRating(Iterator<Rating> ratings) throws IOException {
+    destination.write("\n      <rating value=\"");
+    Rating next = ratings.next();
+    destination.write(Integer.toString(next.value()));
+    destination.write("\" source=\"");
+    destination.write(next.source());
+    destination.write("\" />");
   }
 }
