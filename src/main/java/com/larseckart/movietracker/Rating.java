b037f73d8f7ba23d1c7b574f15da7382c64920e6
Lars Eckart <lars.eckart@gmail.com>
2020-10-20 14:31:32 +0300

    remove unnecessarily specified constructor

--- src/main/java/com/larseckart/movietracker/Rating.java
+++ src/main/java/com/larseckart/movietracker/Rating.java
@@ -7,10 +7,6 @@ record Rating(int value, String source) {
   public Rating(int rating) {
     this(rating, "Anonymous");
   }
-  public Rating(int value, String source) {
-    this.value = value;
-    this.source = source;
-  }
 
   @Override
   public boolean equals(Object o) {
